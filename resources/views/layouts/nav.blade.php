78a04065587c6ca8c837c99798cccf5de2753fc0
Ericka Simancas <ericka.simanca@gmail.com>
2019-05-22 20:54:46 -0400

    a

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -450,23 +450,10 @@
                                     </a>
 
                                     <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <a href="{{ url('users/edit_profile') }}/{{ Auth::user()->id }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-edit mr-1"></i>
                                         <span>Configuración</span>
                                     </a>
-
-                                    <!-- 
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                        <i class="mdi mdi-lifebuoy mr-1"></i>
-                                        <span>Soporte</span>
-                                    </a>-->
-            
-                                    <!-- 
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                        <i class="mdi mdi-lock-outline mr-1"></i>
-                                        <span>Lock Screen</span>
-                                    </a>-->
-
                                     <!-- item-->
                                     <a href="{{ url('users/logout') }}"
                                        onclick="event.preventDefault();
@@ -946,14 +933,14 @@
                                         <h6 class="text-overflow m-0">Bienvenido !</h6>
                                     </div>
 
-                                    <!-- item-->
+                                  <!-- item-->
                                     <a href="{{ url('users/profile') }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-circle mr-1"></i>
                                         <span>Mi cuenta</span>
                                     </a>
 
                                     <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <a href="{{ url('users/edit_profile') }}/{{ Auth::user()->id }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-edit mr-1"></i>
                                         <span>Configuración</span>
                                     </a>
@@ -1372,14 +1359,14 @@
                                         <h6 class="text-overflow m-0">Bienvenido !</h6>
                                     </div>
 
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                   <!-- item-->
+                                    <a href="{{ url('users/profile') }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-circle mr-1"></i>
                                         <span>Mi cuenta</span>
                                     </a>
 
                                     <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <a href="{{ url('users/edit_profile') }}/{{ Auth::user()->id }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-edit mr-1"></i>
                                         <span>Configuración</span>
                                     </a>
@@ -1725,14 +1712,14 @@
                                         <h6 class="text-overflow m-0">Bienvenido !</h6>
                                     </div>
 
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                  <!-- item-->
+                                    <a href="{{ url('users/profile') }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-circle mr-1"></i>
                                         <span>Mi cuenta</span>
                                     </a>
 
                                     <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <a href="{{ url('users/edit_profile') }}/{{ Auth::user()->id }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-edit mr-1"></i>
                                         <span>Configuración</span>
                                     </a>
@@ -2128,14 +2115,14 @@
                                         <h6 class="text-overflow m-0">Bienvenido !</h6>
                                     </div>
 
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                 <!-- item-->
+                                    <a href="{{ url('users/profile') }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-circle mr-1"></i>
                                         <span>Mi cuenta</span>
                                     </a>
 
                                     <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <a href="{{ url('users/edit_profile') }}/{{ Auth::user()->id }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-edit mr-1"></i>
                                         <span>Configuración</span>
                                     </a>
@@ -2555,15 +2542,14 @@
                                     <div class=" dropdown-header noti-title">
                                         <h6 class="text-overflow m-0">Bienvenido !</h6>
                                     </div>
-
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                   <!-- item-->
+                                    <a href="{{ url('users/profile') }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-circle mr-1"></i>
                                         <span>Mi cuenta</span>
                                     </a>
 
                                     <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <a href="{{ url('users/edit_profile') }}/{{ Auth::user()->id }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-edit mr-1"></i>
                                         <span>Configuración</span>
                                     </a>
@@ -2894,15 +2880,14 @@
                                     <div class=" dropdown-header noti-title">
                                         <h6 class="text-overflow m-0">Bienvenido !</h6>
                                     </div>
-
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                   <!-- item-->
+                                    <a href="{{ url('users/profile') }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-circle mr-1"></i>
                                         <span>Mi cuenta</span>
                                     </a>
 
                                     <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <a href="{{ url('users/edit_profile') }}/{{ Auth::user()->id }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-edit mr-1"></i>
                                         <span>Configuración</span>
                                     </a>
@@ -3087,14 +3072,14 @@
                                         <h6 class="text-overflow m-0">Bienvenido !</h6>
                                     </div>
 
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                  <!-- item-->
+                                    <a href="{{ url('users/profile') }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-circle mr-1"></i>
                                         <span>Mi cuenta</span>
                                     </a>
 
                                     <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                    <a href="{{ url('users/edit_profile') }}/{{ Auth::user()->id }}" class="dropdown-item notify-item">
                                         <i class="mdi mdi-account-edit mr-1"></i>
                                         <span>Configuración</span>
                                     </a>
