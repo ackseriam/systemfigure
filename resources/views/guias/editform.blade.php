671529727c567a396aab395b6c3b80992be3a91c
root <root@erickaackseriam>
2019-07-16 21:44:23 -0400

    Arreglos en vista de correccion

--- resources/views/guias/editform.blade.php
+++ resources/views/guias/editform.blade.php
@@ -23,6 +23,7 @@
           <option value="1">Level 1</option>
           <option value="2">Level 2</option>
           <option value="3">Level 3</option>
+          <option value="vpn">VPN</option>
       </select> 
   </div>
      <div class="form-group mb-3">        
